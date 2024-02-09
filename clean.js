7eece8467a2c01890bc2724526731862be1938f2
Alomgir27 <a.h.joy066@gmail.com>
2024-02-09 19:56:58 +0600

    dhfdh

--- clean.js
+++ clean.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Book = require("./models");
+const { User, Station, Train } = require("../models");
 require("dotenv").config();
 
 const dbUrl = process.env.MONGODB_URI;
@@ -9,8 +9,10 @@ mongoose
   .connect(dbUrl)
   .then(async () => {
     console.log("Connected to database!");
-    await Book.deleteMany({});
-    console.log("Database cleared!");
+    await User.deleteMany({});
+    await Station.deleteMany({});
+    await Train.deleteMany({});
+  
     process.exit();
   })
   .catch((error) => {
